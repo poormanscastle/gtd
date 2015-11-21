0299ae3197fd692d84ad863baf695729da434e3a
Georg Federmann <georg.federmann@gmail.com>
2015-11-21 01:03:56 +0100

    implementing activity priorities + unittests

--- src/main/java/at/co/federmann/gtd/domain/Priority.java
+++ src/main/java/at/co/federmann/gtd/domain/Priority.java
@@ -1,8 +1,49 @@
 package at.co.federmann.gtd.domain;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
- * 
+ * the user can define priorities for her actions, tasks, projects, etc, by assigning one
+ * of the enumerated values to the respective item in her GTD.
+ * <p>
  * Created by georg on 21.11.15.
  */
 public enum Priority {
+
+    IGNORE_FOR_NOW(0, "ignore"), LOW(1, "low"), MEDIUM(2, "medium"), HIGH(3, "high"), URGENT(4, "urgent");
+
+    /**
+     * id of the given priority.
+     */
+    private final int id;
+
+    /**
+     * an id that can be used to look up a label for the given priority from some translation tables.
+     */
+    private final String i18nKey;
+
+    private Priority(int id, String i18nKey) {
+        this.id = id;
+        this.i18nKey = i18nKey;
+    }
+
+    public static Priority resolvePriority(Integer id) {
+        if (id == null) {
+            return null;
+        }
+        for (Priority priority : Priority.values()) {
+            if (id.equals(priority.id)) {
+                return priority;
+            }
+        }
+        throw new IllegalArgumentException(StringUtils.join("No matching priority found for id ", id));
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getI18nKey() {
+        return i18nKey;
+    }
 }
---
+++ src/test/java/at/co/federmann/gtd/domain/PriorityTest.java
@@ -0,0 +1,39 @@
+package at.co.federmann.gtd.domain;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Created by georg on 21.11.15.
+ */
+public class PriorityTest {
+
+    @Test
+    public void testResolvePriority() throws Exception {
+        assertTrue(Priority.IGNORE_FOR_NOW == Priority.resolvePriority(0));
+        assertTrue(Priority.LOW == Priority.resolvePriority(1));
+        assertTrue(Priority.MEDIUM == Priority.resolvePriority(2));
+        assertTrue(Priority.HIGH == Priority.resolvePriority(3));
+        assertTrue(Priority.URGENT == Priority.resolvePriority(4));
+    }
+
+    @Test
+    public void testGetId() throws Exception {
+        assertEquals(0, Priority.IGNORE_FOR_NOW.getId());
+        assertEquals(1, Priority.LOW.getId());
+        assertEquals(2, Priority.MEDIUM.getId());
+        assertEquals(3, Priority.HIGH.getId());
+        assertEquals(4, Priority.URGENT.getId());
+    }
+
+    @Test
+    public void testGetI18nKey() throws Exception {
+        assertEquals("ignore", Priority.IGNORE_FOR_NOW.getI18nKey());
+        assertEquals("low", Priority.LOW.getI18nKey());
+        assertEquals("medium", Priority.MEDIUM.getI18nKey());
+        assertEquals("high", Priority.HIGH.getI18nKey());
+        assertEquals("urgent", Priority.URGENT.getI18nKey());
+    }
+}
